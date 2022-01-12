8c1517f67dabdc12e4effc105c19265633b54764
jiawyGithub <973497861@qq.com>
2022-01-12 17:21:09 +0800

    add Logistic Regression

--- 01-Linear Regression/Logistic_Regression.py
+++ 01-Linear Regression/Logistic_Regression.py
@@ -15,11 +15,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
+import torch.nn.functional as F
 
-x_train = [[1.0],[2.0],[3.0]]
-y_train = [[0],[0],[1]]
-x_train = torch.from_numpy(x_train)
-y_train = torch.from_numpy(y_train)
+x_train = torch.Tensor([[1.0],[2.0],[3.0]])
+y_train = torch.Tensor([[0],[0],[1]])
+
+epoch_num = 1000
+learn_rate = 0.01
 
 class LogisticRegressionModel(torch.nn.Module):
     def __init__(self):
@@ -29,4 +31,37 @@ class LogisticRegressionModel(torch.nn.Module):
     def forward(self, x):
         y_pred = F.sigmoid(self.linear(x))
         return y_pred
+
+model = LogisticRegressionModel()
+criterion = torch.nn.BCELoss(size_average=False)
+optimizer = torch.optim.SGD(model.parameters(),lr=learn_rate)
+
+print('model.named_parameters:',list(model.named_parameters()))
+
+for epoch in range(epoch_num):
+    y_pred = model(x_train)
+    loss = criterion(y_pred,y_train)
     
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step() #这个方法会更新所有的参数。一旦梯度被backward后，就可以调用这个函数。
+
+    if (epoch+1) % 20 == 0:
+        print('epoch',epoch+1,'loss',loss.item())
+    
+model.eval()
+with torch.no_grad():
+    x = np.linspace(1, 10, 200)
+    x_t = torch.Tensor(x).view((200, 1))
+    y_t = model(x_t)
+    y = y_t.data.numpy()
+
+# 可视化
+fig = plt.figure(figsize=(10, 5))
+plt.plot(x, y)
+plt.plot([0, 10], [0.5, 0.5], c='r')
+plt.xlabel('Hours')
+plt.ylabel('Probability of Pass')
+# 显示图例
+plt.grid() 
+plt.show()
